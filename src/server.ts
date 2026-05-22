178f046d2aaf8b7e75e3043e3dad3d19003bc238
Shajidaa <shajidaakterlopa101@gmail.com>
2026-05-22 15:37:28 +0600

    project floder setup

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,14 @@
 
-import express, { type Application, type Request, type Response } from 'express'
-const app:Application = express()
-const port = 5000
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use(express.text())
 
-app.get('/', (req:Request, res:Response) => {
-  res.send('Hello World!')
-})
+import app from "./app";
+import config from "./config";
+import { initDB } from "./db";
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const main = () => {
+  initDB();
+  app.listen(config.port, () => {
+    console.log(`Example app listening on port ${config.port}`);
+  });
+};
+
+main();
